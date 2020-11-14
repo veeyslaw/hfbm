854536176bca4c908634f2d79fe91ba120927a4c
veeyslaw <73613949+veeyslaw@users.noreply.github.com>
2020-11-14 23:58:12 +0200

    Test .ui Change

---
+++ hfbm/x64/Debug/uic/ui_gui.h
@@ -0,0 +1,91 @@
+/********************************************************************************
+** Form generated from reading UI file 'gui.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_GUI_H
+#define UI_GUI_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStackedWidget>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_GUIClass
+{
+public:
+    QWidget *centralWidget;
+    QGridLayout *gridLayout;
+    QStackedWidget *stackedWidget;
+    QWidget *mainPage;
+    QWidget *optionsPage;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *GUIClass)
+    {
+        if (GUIClass->objectName().isEmpty())
+            GUIClass->setObjectName(QString::fromUtf8("GUIClass"));
+        GUIClass->resize(900, 700);
+        QFont font;
+        font.setPointSize(16);
+        GUIClass->setFont(font);
+        centralWidget = new QWidget(GUIClass);
+        centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
+        gridLayout = new QGridLayout(centralWidget);
+        gridLayout->setSpacing(6);
+        gridLayout->setContentsMargins(11, 11, 11, 11);
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        stackedWidget = new QStackedWidget(centralWidget);
+        stackedWidget->setObjectName(QString::fromUtf8("stackedWidget"));
+        mainPage = new QWidget();
+        mainPage->setObjectName(QString::fromUtf8("mainPage"));
+        stackedWidget->addWidget(mainPage);
+        optionsPage = new QWidget();
+        optionsPage->setObjectName(QString::fromUtf8("optionsPage"));
+        stackedWidget->addWidget(optionsPage);
+
+        gridLayout->addWidget(stackedWidget, 0, 0, 1, 1);
+
+        GUIClass->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(GUIClass);
+        menuBar->setObjectName(QString::fromUtf8("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 900, 36));
+        GUIClass->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(GUIClass);
+        mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
+        GUIClass->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(GUIClass);
+        statusBar->setObjectName(QString::fromUtf8("statusBar"));
+        GUIClass->setStatusBar(statusBar);
+
+        retranslateUi(GUIClass);
+
+        QMetaObject::connectSlotsByName(GUIClass);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *GUIClass)
+    {
+        GUIClass->setWindowTitle(QCoreApplication::translate("GUIClass", "GUI", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class GUIClass: public Ui_GUIClass {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_GUI_H
