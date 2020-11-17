20ccff02d199805f8232396a381297cb1a2b1964
veeyslaw <73613949+veeyslaw@users.noreply.github.com>
2020-11-18 00:43:29 +0200

    White fkin widget

--- hfbm/Mesh/oglwidget.cpp
+++ hfbm/Mesh/oglwidget.cpp
@@ -2,3 +2,22 @@
 
 OGLWidget::OGLWidget(QWidget* parent)
   : QOpenGLWidget(parent) {}
+
+OGLWidget::~OGLWidget() {}
+
+void OGLWidget::initializeGL()
+{
+  initializeOpenGLFunctions();
+  glClearColor(255, 255, 255, 1);
+  
+}
+
+void OGLWidget::paintGL()
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void OGLWidget::resizeGL(int w, int h)
+{
+
+}
