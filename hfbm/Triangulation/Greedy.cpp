5b3b4dfad5800b833ef1fd1d6a0ddffab8641e40
veeyslaw <73613949+veeyslaw@users.noreply.github.com>
2021-01-12 14:43:16 +0200

    Greedy insert left to do

--- hfbm/Triangulation/Greedy.cpp
+++ hfbm/Triangulation/Greedy.cpp
@@ -2,7 +2,9 @@
 #include <stdexcept>
 #include <iostream>
 
-Greedy::Greedy(std::unique_ptr<HeightMap> heightMap, int meshHeight, double error) : Triangulator(std::move(heightMap), meshHeight, error) {}
+Greedy::Greedy(std::unique_ptr<HeightMap> heightMap, int meshHeight, double error) : Triangulator(std::move(heightMap), meshHeight, error),
+	used(heightMap->getWidth() * heightMap->getHeight(), false)
+{}
 
 void Greedy::run() {
 	if (done) { return; }
@@ -16,50 +18,57 @@ void Greedy::run() {
 	}
 	points.reserve((long long) height * width);
 	triangles.reserve((long long)2 * (height - 1) * (width - 1));
+	maxErrors.reserve((long long)2 * (height - 1) * (width - 1));
+	neighbors.reserve((long long)6 * (height - 1) * (width - 1));
 	
 	// add corner points
 	for (auto y = 0; y < height; y+=height-1) {
 		for (auto x = 0; x < width; x+=width-1) {
 			auto z = heightMap->at((long long)y * width + x);
 			points.push_back(glm::fvec3(x, y, z));
+			used.at(y * width + x) = true;
 		}
 	}
 	// add initial triangles
-	triangles.push_back(Triangle(3, 0, 2));
-	triangles.push_back(Triangle(3, 1, 0));
+	addTriangle(Triangle(3, 0, 2), -1, 1, -1, -1);
+	addTriangle(Triangle(3, 1, 0), -1, -1, -1, 0);
 
-	// add unused points
-	std::vector<std::pair<glm::fvec3, double>> unusedPoints;
-	unusedPoints.reserve((long long)height * width - 4);
-	for (auto y = 0; y < height; y++) {
-		for (auto x = 0; x < width; x++) {
-			if (x == 0 && y == 0 || x == 0 && y == height - 1 || x == width - 1 && y == 0 || x == width - 1 && y == height - 1) {
-				continue;
-			}
-			auto z = heightMap->at((long long)y * width + x);
-			auto point = glm::fvec3(x, y, z);
-			auto error = getError(point);
-			unusedPoints.push_back(std::make_pair(point, error));
-		}
+	for (auto triangle : triangles) {
+		auto maxError = getMaxError(triangle);
+		maxErrors.push_back(maxError);
 	}
-	auto errorComp = [](const std::pair<glm::fvec3, double>& l, const std::pair<glm::fvec3, double>& r)
-											{ return l.second < r.second; };
-	std::make_heap(unusedPoints.begin(), unusedPoints.end(), errorComp);
 
-	while (notReady(unusedPoints)) {
+	while (true) {
 		// insert point with max error in mesh
-		std::pop_heap(unusedPoints.begin(), unusedPoints.end(), errorComp);
-		auto point = unusedPoints.back().first;
-		unusedPoints.pop_back();
-		insert(point);
+		auto max = -1.;
+		glm::ivec2 pointCoord(-1, -1);
+		auto triangleInd = -1;
+		for (auto i = 0; i < maxErrors.size(); i++) {
+			auto errorPair = maxErrors.at(i);
+			if (max < errorPair.first) {
+				max = errorPair.first;
+				pointCoord = errorPair.second;
+				triangleInd = i;
+			}
+		}
+
+		if (max <= error) {
+			break;
+		}
+
+		toUpdate.reserve(triangles.size());
+		insert(pointCoord, triangleInd);
 
 		// update errors
-		for (auto& point: unusedPoints) {
-			point.second = getError(point.first);
+		for (auto triangleInd: toUpdate) {
+			auto maxError = getMaxError(triangles.at(triangleInd));
+			maxErrors.at(triangleInd) = maxError;
 		}
-		std::make_heap(unusedPoints.begin(), unusedPoints.end(), errorComp);
+		toUpdate.clear();
 	}
 
+	neighbors.clear();
+	maxErrors.clear();
 	triangles.shrink_to_fit();
 	points.shrink_to_fit();
 
@@ -76,45 +85,41 @@ void Greedy::run() {
 	done = true;
 }
 
-bool Greedy::notReady(const std::vector<std::pair<glm::fvec3, double>>& unusedPoints) {
-	if (unusedPoints.size() == 0) {
-		return false;
-	}
-	if (unusedPoints.front().second <= error) {
-		return false;
-	}
-	return true;
-}
-
-bool areCollinear(glm::fvec3 p1, glm::fvec3 p2, glm::fvec3 p3) {
-	return p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y) == 0;
-}
-
-bool isOuterEdge(glm::fvec3 p1, glm::fvec3 p2, int w, int h) {
-	return (p1.x == 0 && p2.x == 0 || p1.x == w - 1 && p2.x == w - 1 ||
-		p1.y == 0 && p2.y == 0 || p1.y == h - 1 && p2.y == h - 1);
-}
-
-void Greedy::insert(glm::fvec3 point) {
-	auto newPointIndex = points.size();
-	points.push_back(point);
-	auto p = points.at(newPointIndex);
-	auto triangleIndex = locate(point);
-	auto& triangle = triangles.at(triangleIndex);
-	auto t1 = triangle.points[0];
-	auto p1 = points.at(t1);
-	auto t2 = triangle.points[1];
-	auto p2 = points.at(t2);
-	auto t3 = triangle.points[2];
-	auto p3 = points.at(t3);
+void Greedy::insert(glm::ivec2 point, int triangleIndex) {
+	const auto height = heightMap->getHeight();
+	const auto width = heightMap->getWidth();
+	const auto newPointIndex = points.size();
+	const auto h = heightMap->at(point.y * width + point.x);
+	points.push_back(glm::fvec3(point.x, point.y, h));
+	const auto p = points.at(newPointIndex);
+  auto& triangle = triangles.at(triangleIndex);
+	const auto Aind = triangle.points[0];
+	const auto A = points.at(Aind);
+	const auto Bind = triangle.points[1];
+	const auto B = points.at(Bind);
+	const auto Cind = triangle.points[2];
+	const auto C = points.at(Cind);
+	const auto abn = neighbors.at(triangleIndex);
+	const auto bcn = neighbors.at(triangleIndex + 1);
+	const auto can = neighbors.at(triangleIndex + 2);
 	
+	const auto areCollinear = [](glm::fvec3 p1, glm::fvec3 p2, glm::fvec3 p3) {
+		return p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y) == 0;
+	};
+
+	auto replacedOriginal = false;
+	// check AB
+	if (!areCollinear(A, B, p)) {
+		addTriangle(Triangle(newPointIndex, Aind, Bind), triangleIndex, abn, -1, -1);
+	}
+	else {
+
+	}
+	// check BC
+
+	// check CA
+
 	// split in 3 triangles
-	auto height = heightMap->getHeight();
-	auto width = heightMap->getWidth();
-
-	std::swap(triangles.at(triangleIndex), triangles.at(triangles.size() - 1));
-	triangles.pop_back();
-
 	if (!areCollinear(p1, p2, p)) {
 		triangles.push_back(Triangle(newPointIndex, t1, t2));
 	}
@@ -153,97 +158,38 @@ void Greedy::insert(glm::fvec3 point) {
 			triangles.push_back(Triangle(newPointIndex, opposite.second, t1));
 		}
 	}
-	/*
-	std::vector<Suspect> suspects;
-	// add outer edges as suspects
-	suspects.push_back(Suspect(triangleIndex, t1, t2, newPointIndex));
-	suspects.push_back(Suspect(secondTriangle, t2, t3, newPointIndex));
-	suspects.push_back(Suspect(thirdTriangle, t3, t1, newPointIndex));
-	// check edges and flip if necessary, then check recursively outer edges
-	while (!suspects.empty()) {
-		auto suspect = suspects.back();
-		suspects.pop_back();
-		throw away if on edge
-		auto oppositeSuspect = locate(suspect);
-		auto P = points.at(suspect.point);
-		auto A = points.at(suspect.edge[0]);
-		auto B = points.at(suspect.edge[1]);
-		auto C = points.at(oppositeSuspect.second);
-		// center formula: https://en.wikipedia.org/wiki/Circumscribed_circle
-		B -= A;
-		C -= A;
-		auto factor = 2 * (B.x * C.y - B.y * C.x);
-		auto centerX = (C.y * (B.x * B.x + B.y * B.y) - B.y * (C.x * C.x + C.y * C.y)) / factor + A.x;
-		auto centerY = (B.x * (C.x * C.x + C.y * C.y) - C.x * (B.x * B.x + B.y * B.y)) / factor + A.y;
+}
 
-		auto radiusSq = centerX * centerX + centerY * centerY;
+std::pair<double, glm::ivec2> Greedy::getMaxError(Triangle t) {
+	// precalculations
+	const auto& A = points.at(t.points[0]);
+	const auto& B = points.at(t.points[1]);
+	const auto& C = points.at(t.points[2]);
 
-		auto xDistSq = centerX - P.x;
-		xDistSq *= xDistSq;
-		auto yDistSq = centerY - P.y;
-		yDistSq *= yDistSq;
-		if (xDistSq + yDistSq <= radiusSq) {
-			// confirmed suspect
-			for (auto& point : triangles.at(suspect.triangle).points) {
-				if (point == suspect.point) {
-					point = wrong;
-				}
-			}
+	// for pointInTriangle
+	const double s1 = C.y - A.y;
+	const double s2 = C.x - A.x;
+	const double s3 = B.y - A.y;
+	const double s5 = A.x * s1;
+	const double s6 = s3 * s2;
+	const double s7 = B.x - A.x;
+
+	const auto pointInTriangle = [=](glm::ivec2 point)
+	{
+		double s4 = point.y - A.y;
+		double w1, w2;
+		if (s1 != 0) {
+			w1 = (s5 + s4 * s2 - point.x * s1) / (s6 - s7 * s1);
+			w2 = (s4 - w1 * s3) / s1;
 		}
-	}
-	*/
-}
-
-int Greedy::locate(glm::fvec3 point) {
-	for (auto i = 0; i < triangles.size(); i++) {
-		if (pointInTriangle(point, i)) {
-			return i;
-			break;
+		else {
+			w1 = s4 / s3;
+			w2 = (point.x - A.x - w1 * s7) / s2;
 		}
-	}
+		return w1 >= 0 && w2 >= 0 && (w1 + w2) <= 1;
+	};
 
-	return -1;
-}
-
-std::pair<int, int> Greedy::locate(Suspect suspect) {
-	// find triangle that shares the edge
-	for (auto i = 0; i < triangles.size(); i++) {
-		const auto& triangle = triangles.at(i);
-		auto p1 = triangle.points[0];
-		auto p2 = triangle.points[1];
-		auto p3 = triangle.points[2];
-		if (suspect.point != p1 && suspect.point != p2 && suspect.point != p3 &&
-			(suspect.edge[0] == p1 || suspect.edge[0] == p2 || suspect.edge[0] == p3) &&
-			(suspect.edge[1] == p1 || suspect.edge[1] == p2 || suspect.edge[1] == p3)
-		) {
-			auto oppositePoint = p1 != suspect.edge[0] && p1 != suspect.edge[1] ? p1 :
-				p2 != suspect.edge[0] && p2 != suspect.edge[1] ? p2 :
-				p3;
-			return std::make_pair(i, oppositePoint);
-			break;
-		}
-	}
-	throw "triangle location went wrong";
-}
-
-double Greedy::getError(glm::fvec3 point) {
-	try {
-		auto interpolation = std::abs(heightMap->at(point.y * heightMap->getWidth() + point.x) - interpolate(point)) / meshHeight;
-		auto wtf = heightMap->at(point.y * heightMap->getWidth() + point.x);
-		auto wtf2 = interpolate(point);
-		return interpolation;
-	}
-	catch(std::out_of_range oob) {
-		return 0;
-	}
-}
-
-double Greedy::interpolate(glm::fvec3 point) {
-	auto triangleIndex = locate(point);
-	const auto& triangle = triangles.at(triangleIndex);
-	const auto& A = points.at(triangle.points[0]);
-	const auto& B = points.at(triangle.points[1]);
-	const auto& C = points.at(triangle.points[2]);
+	// for interpolation
 	// find normal vector to the plane
 	auto AB = B - A;
 	auto AC = C - A;
@@ -253,29 +199,57 @@ double Greedy::interpolate(glm::fvec3 point) {
 	// point = (x, y, z)
 	// plane equation given A and n: a*(x-x0) + b*(y-y0) + c*(z-z0) = 0
 	// interpolation = z = ...
-	return (double)n.x * (A.x - point.x) / n.z + (double)n.y * (A.y - point.y) / n.z + A.z;
+
+	const auto interpolate = [=](glm::ivec2 point) {
+		return (double)n.x * (A.x - point.x) / n.z + (double)n.y * (A.y - point.y) / n.z + A.z;
+	};
+
+	const glm::fvec3 minf = glm::min(glm::min(A, B), C);
+	const glm::ivec2 min(minf.x, minf.y);
+	const glm::fvec3 maxf = glm::max(glm::max(A, B), C);
+	const glm::ivec2 max(maxf.x, maxf.y);
+
+	auto maxError = -1.;
+	glm::ivec2 maxCoord(-1, -1);
+	for (auto y = min.y; y <= max.y; y++) {
+		for (auto x = min.x; x <= max.x; x++) {
+			glm::ivec2 point(x, y);
+			if (!pointInTriangle(point)) {
+				continue;
+			}
+
+			auto h = heightMap->at(y * heightMap->getWidth() + x);
+			auto error = -1.;
+			try {
+				auto interpolation = interpolate(point);
+				error = (h - interpolation) / meshHeight;
+			}
+			catch (std::out_of_range oob) {
+				// data related errors
+			}
+			if (error > maxError) {
+				maxError = error;
+				maxCoord = point;
+			}
+		}
+	}
+
+	return std::make_pair(maxError, maxCoord);
 }
 
-bool Greedy::pointInTriangle(glm::fvec3 point, int triangleIndex)
-{
-	// adapted version of: https://github.com/SebLague/Gamedev-Maths/blob/master/PointInTriangle.cs
-	const auto& triangle = triangles.at(triangleIndex);
-	const auto& A = points.at(triangle.points[0]);
-	const auto& B = points.at(triangle.points[1]);
-	const auto& C = points.at(triangle.points[2]);
-	
-	double s1 = C.y - A.y;
-	double s2 = C.x - A.x;
-	double s3 = B.y - A.y;
-	double s4 = point.y - A.y;
-	double w1, w2;
-	if (s1 != 0) {
-		w1 = (A.x * s1 + s4 * s2 - point.x * s1) / (s3 * s2 - (B.x - A.x) * s1);
-		w2 = (s4 - w1 * s3) / s1;
+void Greedy::addTriangle(Triangle t, int ind, int abn, int bcn, int can) {
+	if (ind < 0) {
+		ind = triangles.size();
+		triangles.push_back(t);
+		neighbors.push_back(abn);
+		neighbors.push_back(bcn);
+		neighbors.push_back(can);
 	}
 	else {
-		w1 = s4 / s3;
-		w2 = (point.x - A.x - w1 * (B.x - A.x)) / (C.x - A.x);
+		std::swap(triangles.at(ind), t);
+		neighbors.at(ind) = abn;
+		neighbors.at(ind + 1) = bcn;
+		neighbors.at(ind + 2) = can;
 	}
-	return w1 >= 0 && w2 >= 0 && (w1 + w2) <= 1;
+	toUpdate.push_back(ind);
 }
--- hfbm/Triangulation/Greedy.h
+++ hfbm/Triangulation/Greedy.h
@@ -4,27 +4,20 @@
 #include "Triangulator.h"
 #include "../Mesh/Mesh.h"
 
-struct Suspect {
-  Suspect(int triangle, int point1, int point2, int point) :
-    triangle(triangle), edge({ point1, point2 }), point(point) {}
-
-  int triangle;
-  int point;
-  std::array<int, 2> edge;
-};
-
 class Greedy :
     public Triangulator
 {
 public:
   Greedy(std::unique_ptr<HeightMap>, int, double);
   void run() override;
-  void insert(glm::fvec3);
-  int locate(glm::fvec3);
-  std::pair<int, int> locate(Suspect);
-  bool notReady(const std::vector<std::pair<glm::fvec3, double>>&);
-  double getError(glm::fvec3);
-  double interpolate(glm::fvec3);
-  bool pointInTriangle(glm::fvec3, int);
+  void insert(glm::ivec2, int);
+  void addTriangle(Triangle, int, int, int, int);
+  std::pair<double, glm::ivec2> getMaxError(Triangle);
+
+protected:
+  std::vector<std::pair<double, glm::ivec2>> maxErrors;
+  std::vector<int> toUpdate;
+  std::vector<int> neighbors;
+  std::vector<bool> used;
 };
 
